25c3b2d88fac89247263f501e4d325b02e954615
Adam <adamaly93@gmail.com>
2025-10-28 11:28:24 +0000

    [quartus] add setup files

--- ocram_32k.vhd
+++ ocram_32k.vhd
@@ -78,7 +78,7 @@ BEGIN
 		clock_enable_output_a => "BYPASS",
 		clock_enable_output_b => "BYPASS",
 		indata_reg_b => "CLOCK0",
-		init_file => "",
+		init_file => "../blinker.hex",
 		intended_device_family => "Cyclone III",
 		lpm_type => "altsyncram",
 		numwords_a => 8192,
@@ -155,7 +155,7 @@ END SYN;
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 -- Retrieval info: PRIVATE: MEMSIZE NUMERIC "262144"
 -- Retrieval info: PRIVATE: MEM_IN_BITS NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING ""
+-- Retrieval info: PRIVATE: MIFfilename STRING "../blinker.hex"
 -- Retrieval info: PRIVATE: OPERATION_MODE NUMERIC "3"
 -- Retrieval info: PRIVATE: OUTDATA_ACLR_B NUMERIC "0"
 -- Retrieval info: PRIVATE: OUTDATA_REG_B NUMERIC "0"
@@ -191,7 +191,7 @@ END SYN;
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_B STRING "BYPASS"
 -- Retrieval info: CONSTANT: INDATA_REG_B STRING "CLOCK0"
--- Retrieval info: CONSTANT: INIT_FILE STRING ""
+-- Retrieval info: CONSTANT: INIT_FILE STRING "../blinker.hex"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone III"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
 -- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "8192"
--- reset_control.v
+++ reset_control.v
@@ -1,16 +1,21 @@
 module reset_control (
     input wire clk,
-    input wire reset_button_n,
-    output reset_n
+    input wire reset_button_n, 
+    output wire reset_n 
 );
 
-  reg [5:0] reset_count = 0;
+   reg [10:0] reset_count = 0; 
+    
+	assign reset_n = &reset_count;
+	 
+	always @(posedge clk) begin
+		 if (~reset_button_n) begin 
+			  reset_count <= 'b0;
 
-  assign reset_n = &reset_count;
-
-  always @(posedge clk) begin
-    if (reset_button_n) reset_count <= reset_count + !reset_n;
-    else reset_count <= 'b0;
-  end
-
+		 end
+		 else begin 
+			  reset_count <= reset_count + !reset_n;
+		 end
+	end
+    
 endmodule
